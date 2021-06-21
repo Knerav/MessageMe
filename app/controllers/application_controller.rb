0036ce0f530596ec2ed512d155aaf9760c05f721
Knerav Kodolikar <knerav@gmail.com>
2021-06-21 11:15:52 +0530

    added controller restrictions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ class ApplicationController < ActionController::Base
 
   def require_user
     if !logged_in?
-      flash[:error] = "You must be logged in to perform this action"
+      flash[:warning] = "You must be logged in to perform this action"
       redirect_to login_path
     end
   end
